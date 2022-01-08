2f70c1bc4fc1c9a76af2180ba952c8612af4aa81
Itay Lisaey <et030102@gmail.com>
2022-01-08 15:32:44 +0200

    feat(donate): added donatino text and some style

--- src/constants/donate.constant.ts
+++ src/constants/donate.constant.ts
@@ -1,4 +1,5 @@
 export const DONATE_CONST = {
-    title: 'Hey, consider donating',
+    title: 'Hey, consider supporting "Stick Around"',
     link: 'https://paypal.me/ItayLisaey?country.x=IL&locale.x=en_US',
+    para: 'If ypur like te app, consider supporting us in making the app better and keepin it free :) \n\nAs a community driven, open-sourced, ad-less, free app, "Stick Around" generates no revnue and thus need your support to keep going.',
 };
