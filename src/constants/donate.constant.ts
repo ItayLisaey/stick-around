ab38c2b3fde21c5102d251457c294dfa41308af0
Itay Lisaey <et030102@gmail.com>
2022-01-08 15:35:30 +0200

    fix(donation): fixed text better englosh

--- src/constants/donate.constant.ts
+++ src/constants/donate.constant.ts
@@ -1,5 +1,8 @@
 export const DONATE_CONST = {
     title: 'Hey, consider supporting "Stick Around"',
     link: 'https://paypal.me/ItayLisaey?country.x=IL&locale.x=en_US',
-    para: 'If ypur like te app, consider supporting us in making the app better and keepin it free :) \n\nAs a community driven, open-sourced, ad-less, free app, "Stick Around" generates no revnue and thus need your support to keep going.',
+    para:
+        'If you liked the app, consider supporting us in making the app better and keepin' +
+        "'" +
+        ' it free :) \n\nAs a community driven, open-sourced, ad-less, free app, "Stick Around" generates no revenue and thus needs your support to keep going.',
 };
